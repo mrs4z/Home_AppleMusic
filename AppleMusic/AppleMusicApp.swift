5c65477855cc104b503b92c31f5d15a459fdfbe7
mrs4z <mrs4z@icloud.com>
2021-12-04 19:38:50 +0300

    Initial Commit

---
+++ AppleMusic/AppleMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicApp.swift
+//  AppleMusic
+//
+//  Created by Александр Горденко on 04.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
