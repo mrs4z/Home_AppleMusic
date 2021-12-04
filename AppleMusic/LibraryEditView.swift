ea86ae04249f453a3671a9e7da3c774fc01681b0
mrs4z <mrs4z@icloud.com>
2021-12-05 01:32:30 +0300

    Init library main view
    - Add tabs
    - Add player
    - Add navigationBar
    - Setup edit view

---
+++ AppleMusic/LibraryEditView.swift
@@ -0,0 +1,20 @@
+//
+//  LibraryEditView.swift
+//  AppleMusic
+//
+//  Created by Александр Горденко on 05.12.2021.
+//
+
+import SwiftUI
+
+struct LibraryEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LibraryEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        LibraryEditView()
+    }
+}
---
+++ AppleMusic/LibraryView.swift
@@ -0,0 +1,49 @@
+//
+//  LibraryView.swift
+//  AppleMusic
+//
+//  Created by Александр Горденко on 04.12.2021.
+//
+
+import SwiftUI
+
+struct LibraryView: View {
+    @State var isEdit = false
+    var body: some View {
+        NavigationView {
+            GeometryReader { geometry in
+                ZStack {
+                    ScrollView(.vertical) {
+                        if isEdit {
+                            LibraryEditView()
+                        } else {
+                            VStack {
+                                LibraryMainView()
+                            }
+                            .frame(width: geometry.size.width)
+                            .frame(minHeight: geometry.size.height)
+                            
+                        }
+                        
+                    }
+                    .navigationTitle("Медиатека")
+                    .navigationBarItems(trailing:
+                                            Button(action: {
+                        isEdit = !isEdit
+                    }, label: {
+                        Text(isEdit ? "Править" : "Готово")
+                    }))
+                    PlayerView()
+                        .offset(x: 0, y: (geometry.size.height / 2) - 38)
+                }
+            }
+        }
+    }
+}
+
+
+struct LibraryView_Previews: PreviewProvider {
+    static var previews: some View {
+        LibraryView()
+    }
+}
